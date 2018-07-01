625d0d7aedb43a720ee6a515fe52e4a16ca6545a
muslimipro <muslim.ipro@gmail.com>
2018-07-01 11:32:50 +0600

    add destory dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	belongs_to :token, class_name: "Token"
+	belongs_to :token
 
 	validates_presence_of :name, :surname, :token, :mac, :time
 	validates_uniqueness_of :mac
